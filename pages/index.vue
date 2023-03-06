1a2c6d49758a18cff9526043172fbb74e2eb9152
Maxwell Muhanda <ajaybullec@gmail.com>
2023-03-06 23:48:09 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<header>
+			<nav>
+				<NuxtLink to="/">Nuxt Dojo</NuxtLink>
+				<ul>
+					<li><NuxtLink to="/">Home</NuxtLink></li>
+					<li><NuxtLink to="/about">About</NuxtLink></li>
+					<li><a href="/about">Normal About</a></li>
+					<li><NuxtLink to="/products">Products</NuxtLink></li>
+				</ul>
+			</nav>
+		</header>
+		<h1>Home</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, cum.
+			Quasi omnis blanditiis facere! Culpa impedit temporibus aperiam iste
+			quasi.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, cum.
+			Quasi omnis blanditiis facere! Culpa impedit temporibus aperiam iste
+			quasi.
+		</p>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h1 {
+	margin-bottom: 20px;
+	font-size: 30px;
+}
+p {
+	margin: 20px 0;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<p>Product detail for {{ id }}</p>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam labore
+			quod cum, in dolor esse? Facilis voluptatem saepe placeat harum?
+		</p>
+	</div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
+
+<style scoped></style>
